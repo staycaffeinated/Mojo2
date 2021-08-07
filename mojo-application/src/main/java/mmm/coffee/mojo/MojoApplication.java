3d3db1d60683ff06213b238cb2f1601da2103d47
Jon Caulfield <danesub@yahoo.com>
2021-08-07 07:12:18 -0500

    chore: update version string to 0.2.1

--- mojo-application/src/main/java/mmm/coffee/mojo/MojoApplication.java
+++ mojo-application/src/main/java/mmm/coffee/mojo/MojoApplication.java
@@ -26,7 +26,7 @@ import java.util.concurrent.Callable;
 @CommandLine.Command(
         name="mojo",
         description="Code generation mojo for programmers",
-        version = "0.2.0",
+        version = "0.2.1",
         mixinStandardHelpOptions = true,
         subcommands = { CommandRestApi.class }
 )
